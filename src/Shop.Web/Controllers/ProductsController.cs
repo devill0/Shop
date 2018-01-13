3f81e293587a5720bb6d0026dcfd87844fe23fce
Marcin Aleksander Szatan <mszatan@student.wszib.edu.pl>
2018-01-13 12:55:32 +0100

    Added new View with Bootstrap

---
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+
+namespace Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static readonly List<Product> products = new List<Product>
+        {
+            new Product("Laptop", "Electronics", 3000),
+            new Product("Jeans", "Trousers", 150),
+            new Product("Hammer", "Tools", 50),
+            new Product("Ticket", "Cinema", 20)
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
