372e63a93d50e6122408e520ea7c33dac41395df
Marcin Aleksander Szatan <mszatan@student.wszib.edu.pl>
2018-01-13 15:05:29 +0100

    Added Product model and view. Modified Products controller/view.

--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
+using Shop.Web.Models;
 
 namespace Shop.Web.Controllers
 {
+    [Route("products")]
     public class ProductsController : Controller
     {
         private static readonly List<Product> products = new List<Product>
@@ -17,9 +19,31 @@ namespace Shop.Web.Controllers
             new Product("Ticket", "Cinema", 20)
         };
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View(products);
+        }
+
+        [HttpGet("add")]
+        public IActionResult AddProduct()
+        {
+            var viewModel = new ProductViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddProduct(ProductViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            products.Add(new Product(viewModel.Name, viewModel.Category, viewModel.Price));
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
