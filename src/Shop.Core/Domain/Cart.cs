275da68cb5fcc09203b33e063bddcf04b1446a67
Marcin Aleksander Szatan <mszatan@student.wszib.edu.pl>
2018-01-27 09:24:32 +0100

    Added Cart and CartItem

---
+++ src/Shop.Core/Domain/Cart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Core.Domain
+{
+    public class Cart
+    {
+        private ISet<CartItem> items = new HashSet<CartItem>();
+        public IEnumerable<CartItem> Items => items;
+        public decimal TotalPrice => Items.Sum(i => i.TotalPrice);
+
+        public void AddProduct(Product product)
+        {
+            var item = items.SingleOrDefault(x => x.ProductId == product.Id);
+            if (item == null)
+            {
+                item = new CartItem(product);
+                items.Add(item);
+
+                return;
+            }
+            item.IncreaseQuantity();
+        }
+
+        public void DeleteProduct(Guid productId)
+        {
+            var item = items.SingleOrDefault(x => x.ProductId == productId);
+
+            if(item == null)
+            {
+                throw new Exception($"Product with id: '{productId}' was not found in cart.");
+            }
+            if(item.Quantity == 1)
+            {
+                items.Remove(item);
+
+                return;
+            }
+            item.DecreaseQuantity();
+        }
+
+        public void Clear() => items.Clear();
+    }
+}
